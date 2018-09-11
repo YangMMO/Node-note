42b2f020b886cc55c7de2742ddde1008d54a7a02
YangMMO <11436052@qq.com>
2018-09-11 17:30:53 +0800

    push

--- P7/express/myapp/routes/index.js
+++ P7/express/myapp/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.param('name'))
+  res.render('index', { title: req.param('name') });
 });
 
 module.exports = router;
