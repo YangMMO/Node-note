25154b8fb7d4d9d62bf228114b63ec830931aebb
YangMMO <11436052@qq.com>
2018-09-12 17:57:20 +0800

    push

--- P7/pm2/app/routes/index.js
+++ P7/pm2/app/routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var count = 0;
+// var count = 0;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(count >= 2) {
-    process.exit();
-  }
-  ++ count;
+  // if(count >= 2) {
+  //   process.exit();
+  // }
+  // ++ count;
   res.render('index', { title: 'Express' });
 });
 
